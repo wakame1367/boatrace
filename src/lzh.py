e36085de635aad1ab592e25669c1016f807fe256
kot.yuhara <kot.yuhara@trifort.jp>
2019-07-17 15:11:35 +0900

    init

---
+++ src/lzh.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+import lhafile
+
+
+# https://trac.neotitans.net/wiki/lhafile/
+def unlzh(lzh_name):
+    """Extract files under current directory"""
+    print("Extract", lzh_name, "...")
+    if not isinstance(lzh_name, Path):
+        raise ValueError("{} is not supported.".format(type(lzh_name)))
+    if not lzh_name.suffix == ".lzh":
+        raise ValueError("{} is not supported extension.".format(lzh_name.suffix))
+    # make directory to extract
+    if not lzh_name.parent.exists():
+        lzh_name.parent.mkdir()
+
+    # open lzh file and get file names in it.
+    lha = lhafile.Lhafile(lzh_name)
+
+    # extract all files
+    for lha_info in lha.infolist():
+        file_path = lha_info.filename
+        print(file_path)
+        print(type(file_path))

ea62990790e68c673e511e05a6244e64820bec80
kot.yuhara <kot.yuhara@trifort.jp>
2019-07-17 15:26:05 +0900

    init

---
+++ run.py
@@ -0,0 +1,19 @@
+import argparse
+
+from src.lzh import unlzh
+
+
+def get_arguments():
+    _parser = argparse.ArgumentParser()
+    _parser.add_argument("lzh_path", type=str)
+    _args = _parser.parse_args()
+    return _args
+
+
+def main():
+    args = get_arguments()
+    unlzh(args.lzh_path)
+
+
+if __name__ == '__main__':
+    main()

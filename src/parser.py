99ac48eebde55c99132010955e3f98276e318e75
wakamezake <hotwater1367@gmail.com>
2019-07-19 01:38:35 +0900

    update

--- src/parser.py
+++ src/parser.py
@@ -4,9 +4,15 @@ class Result:
                      "type",
                      "landing_boat", "registration_number", "player_name",
                      "ﾓｰﾀｰ", "ﾎﾞｰﾄ", "展示", "進入", "ｽﾀｰﾄﾀｲﾐﾝｸ", "ﾚｰｽﾀｲﾑ"]
-        self.header = ["idx", "landing_boat", "registration_number",
-                       "player_name", "mortar", "board", "exhibition",
-                       "approach", "race_time"]
+        self.race_info_header = ["round", "race_name", "rave_type",
+                                 "course_length", "weather", "wind",
+                                 "wind_direction", "wind_speed", "wave",
+                                 "wave_height"]
+        self.race_result_header = ["idx", "landing_boat",
+                                   "registration_number",
+                                   "player_name", "mortar", "board",
+                                   "exhibition",
+                                   "approach", "race_time"]
         self.dtypes = [int, int, int, str, int, int, float, int, float, str]
         self.sep_size = 79
         self.separator = "-" * self.sep_size
@@ -29,5 +35,16 @@ class Result:
         new_txt = []
         for line in txt:
             rline = line.strip().replace("\u3000", "").replace(".  .", "-")
-            new_txt.append(rline.split())
-        return new_txt
+            split_line = rline.split()
+            if len(split_line) == 9:
+                split_line.insert(self.race_info_length, "-")
+            new_txt.append(split_line)
+
+        start_line = new_txt[0]
+        lines = []
+        for line_no, line in enumerate(new_txt):
+            if line_no % (self.race_result_length + 1) == 0:
+                start_line = line
+            else:
+                lines.append(start_line + line)
+        return lines

968cef3f58df4b85440ccc262025343c1bd621c3
kot.yuhara <kot.yuhara@trifort.jp>
2019-07-29 18:42:15 +0900

    add anomaly landing flag

--- src/parser.py
+++ src/parser.py
@@ -23,7 +23,16 @@ class Result:
         self.race_result_length = 6
         # Reference
         # http://boat-advisor.com/soft/manual/data_keyword.htm
-        self.is_landing_boat = re.compile("0[1-6]|S[0-2]|L[0-1]|K[0-1]|F")
+        self.landing_boat_pattern = re.compile(r"0[1-6]")
+        self.disqualification_pattern = re.compile(r"S[0-2]")
+        self.false_start_pattern = re.compile(r"F")
+        self.delay_pattern = re.compile(r"L[0-1]")
+        self.miss_race_pattern = re.compile(r"K[0-1]")
+        self.landing_boat_pattern = re.compile("|".join(pat.pattern for pat in [self.landing_boat_pattern,
+                                                                                self.disqualification_pattern,
+                                                                                self.false_start_pattern,
+                                                                                self.delay_pattern,
+                                                                                self.miss_race_pattern]))
 
     def parse(self, path):
         sep_index = []
@@ -54,8 +63,10 @@ class Result:
                 # http://boat-advisor.com/soft/manual/data_keyword.htm
                 # 着順情報が 01 ~ 06 以外の情報が存在する
                 # 観測できたのは K0 : 欠場, 欠場の場合race_time等が歯抜けになる
+                is_anomaly_landing = False
                 if not re.match("0[1-6]", split_line[0]):
-                    print(split_line)
+                    is_anomaly_landing = True
+                split_line.append(is_anomaly_landing)
             new_txt.append(split_line)
 
         start_line = new_txt[0]
@@ -70,14 +81,14 @@ class Result:
 
     def is_race_result(self, line):
         landing_boat = line[0]
-        if self.is_landing_boat.match(landing_boat):
+        if self.landing_boat_pattern.match(landing_boat):
             return True
         else:
             return False
 
     def is_race_info(self, line):
         landing_boat = line[0]
-        if self.is_landing_boat.match(landing_boat):
+        if self.landing_boat_pattern.match(landing_boat):
             return False
         else:
             return True
